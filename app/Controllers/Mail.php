d23da40215ed913e33aefec837da04d29ea7060d
Zed3 <shaishofet@gmail.com>
2015-07-26 02:21:34 +0300

    trying to solve mail issues

--- app/Controllers/Mail.php
+++ app/Controllers/Mail.php
@@ -8,12 +8,19 @@ use Helpers\SimpleCurl as Curl;
 class Mail extends Controller {
 
 	function test() {
-		mail('shaishofet@gmail.com', 'test from php', 'test from php');
-	    $mail = new \Helpers\PhpMailer\mail();
-	    $mail->setFrom('noreply@domain.com');
-	    $mail->addAddress('shaishofet@gmail.com');
-	    $mail->subject('Important Email');
-	    $mail->body("<h1>Hey</h1><p>I like this <b>Bold</b> Text!</p>");
-	    $mail->send();
+		$to      = 'shaishofet@gmail.com';
+		$subject = 'the subject';
+		$message = 'hello';
+		$headers = 'From: foodie@zed3.us' . "\r\n" .
+		    'Reply-To: foodie@zed3.us' . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
+
+		mail($to, $subject, $message, $headers);
+	    // $mail = new \Helpers\PhpMailer\mail();
+	    // $mail->setFrom('noreply@domain.com');
+	    // $mail->addAddress('shaishofet@gmail.com');
+	    // $mail->subject('Important Email');
+	    // $mail->body("<h1>Hey</h1><p>I like this <b>Bold</b> Text!</p>");
+	    // $mail->send();
 	}
 }
