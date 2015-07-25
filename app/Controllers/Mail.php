a805640bf0b3136a645bfc0d25f3d54885ee2d9f
Zed3 <shaishofet@gmail.com>
2015-07-26 02:29:49 +0300

    trying to solve mail issues

--- app/Controllers/Mail.php
+++ app/Controllers/Mail.php
@@ -15,12 +15,12 @@ class Mail extends Controller {
 		    'Reply-To: foodie@zed3.us' . "\r\n" .
 		    'X-Mailer: PHP/' . phpversion();
 
-		// mail($to, $subject, $message, $headers);
-	 //    $mail = new \Helpers\PhpMailer\mail();
-	 //    $mail->setFrom('foodie@zed3.us');
-	 //    $mail->addAddress('shaishofet@gmail.com');
-	 //    $mail->subject('Important Email');
-	 //    $mail->body("<h1>Hey</h1><p>I like this <b>Bold</b> Text!</p>");
-	 //    var_dump($mail->send());
+		mail($to, $subject, $message, $headers);
+	    // $mail = new \Helpers\PhpMailer\mail();
+	    // $mail->setFrom('foodie@zed3.us');
+	    // $mail->addAddress('shaishofet@gmail.com');
+	    // $mail->subject('Important Email');
+	    // $mail->body("<h1>Hey</h1><p>I like this <b>Bold</b> Text!</p>");
+	    // var_dump($mail->send());
 	}
 }
