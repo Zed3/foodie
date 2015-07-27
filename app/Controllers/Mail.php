5106a142e72ce457d9dd9dca11191db269c73905
Zed3 <shaishofet@gmail.com>
2015-07-27 21:49:12 +0300

    Added mail feature to ping, WIP

--- app/Controllers/Mail.php
+++ app/Controllers/Mail.php
@@ -27,11 +27,24 @@ class Mail extends Controller {
 
 	function send_delivery($delivery) {
 		$to      = 'shaishofet@gmail.com';
-		$subject = "[Food] המשלוח ממסעדת $delivery->rest_name סופק לחברה";
-		$message = 'Send via <a href="">Foodie</a>';
+		$subject = "[Food] $delivery->rest_name";
+
+$message = '<html><body>';
+$message .= "<h3>המשלוח ממסעדת $delivery->rest_name סופק לחברה</h3>";
+$message .= '<p>Send via <a href="http://foodie.zed3.us">Foodie</a></p>';
+$message .= '</body></html>';		
+
 		$headers = 'From: foodie@zed3.us' . "\r\n" .
 		    'Reply-To: foodie@zed3.us' . "\r\n" .
 		    'X-Mailer: PHP/' . phpversion();
+
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+// Additional headers
+$headers .= 'From: Foodie <foodie@zed3.us>' . "\r\n";
 
 		mail($to, $subject, $message, $headers);
 	    // $mail = new \Helpers\PhpMailer\mail();
