cc2c97cd266122cae0c7d6a48ce6573b4b4d86ea
Zed3 <shaishofet@gmail.com>
2015-07-27 21:51:49 +0300

    Added mail feature to ping, WIP

--- app/Controllers/Mail.php
+++ app/Controllers/Mail.php
@@ -28,24 +28,20 @@ class Mail extends Controller {
 	function send_delivery($delivery) {
 		$to      = 'shaishofet@gmail.com';
 		$subject = "[Food] $delivery->rest_name";
-print_r($delivery);
-die();
-$message = '<html><body>';
-$message .= "<h3>המשלוח ממסעדת $delivery->rest_name סופק לחברה</h3>";
-$message .= '<p>Send via <a href="http://foodie.zed3.us">Foodie</a></p>';
-$message .= '</body></html>';		
 
-		$headers = 'From: foodie@zed3.us' . "\r\n" .
-		    'Reply-To: foodie@zed3.us' . "\r\n" .
-		    'X-Mailer: PHP/' . phpversion();
-
+		$message = '<html><body>';
+		$message .= "<h3>המשלוח ממסעדת $delivery->rest_name סופק לחברה</h3>";
+		$message .= '<p>Send via <a href="http://foodie.zed3.us">Foodie</a></p>';
+		$message .= '</body></html>';		
 
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 // Additional headers
-$headers .= 'From: Foodie <foodie@zed3.us>' . "\r\n";
+		$headers .= 'From: foodie@zed3.us' . "\r\n" .
+		    'Reply-To: foodie@zed3.us' . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
 
 		mail($to, $subject, $message, $headers);
 	    // $mail = new \Helpers\PhpMailer\mail();
