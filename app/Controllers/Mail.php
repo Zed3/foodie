46e582e3319d1002ee49f638df575dbde22388fa
Zed3 <shaishofet@gmail.com>
2015-07-27 22:03:41 +0300

    Added mail feature to ping, WIP

--- app/Controllers/Mail.php
+++ app/Controllers/Mail.php
@@ -27,6 +27,7 @@ class Mail extends Controller {
 
 	function send_delivery($delivery) {
 		$to      = 'shai@il.ibm.com';
+		$to      = 'shaishofet@gmail.com';
 		$subject = "[Food] $delivery->rest_name";
 
 		$message = '<html><body>';
@@ -43,13 +44,33 @@ $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 		    'Reply-To: foodie@zed3.us' . "\r\n" .
 		    'X-Mailer: PHP/' . phpversion();
 
-		mail($to, $subject, $message, $headers);
-	    // $mail = new \Helpers\PhpMailer\mail();
-	    // $mail->setFrom('foodie@zed3.us');
-	    // $mail->addAddress('shaishofet@gmail.com');
-	    // $mail->subject('Important Email');
-	    // $mail->body("<h1>Hey</h1><p>I like this <b>Bold</b> Text!</p>");
-	    // var_dump($mail->send());
+//		mail($to, $subject, $message, $headers);
+
+$mail = new \Helpers\PhpMailer\mail();
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; // or 587
+$mail->IsHTML(true);
+$mail->Username = "trusteerfoodie@gmail.com";
+$mail->Password = "thisismygreatpassword";
+$mail->SetFrom("trusteerfoodie@gmail.com");
+$mail->Subject = $subject;
+$mail->Body = "hello";
+$mail->AddAddress("shaishofet@gmail.com");
+ if(!$mail->Send())
+    {
+    	echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+    else
+    {
+    	echo "Message has been sent";
+    }
+
+
+
 	}
 
 
